9256d07c3b45fff673dc69cb7cda8f6df1e0859a
G G <cwguy64@hotmail.com>
2020-04-18 21:25:09 -0400

    using dbConfig.js

--- verse.js
+++ verse.js
@@ -1,18 +1,14 @@
 const https = require('https');
 const fs = require('fs');
 
-const oracledb = require('oracledb');
+const oracledb = require('oracledb'), dbConfig = require("./dbconfig.js");;
 
 async function run() {
  let connection;
  
  try {
-    connection = await oracledb.getConnection({
-     user: process.env.NODE_ORACLEDB_USER,
-     password: process.env.NODE_ORACLEDB_PASSWORD,
-     connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING
-   });
-   let result = await connection.execute("select sysdate from dual");
+    connection = await oracledb.getConnection({user: dbConfig.user, password: dbConfig.password, connectString: dbConfig.connectString});
+   let result = await connection.execute("select count(*) from verse where contains(content,'twenty')>0");
    console.log(result.rows[0]);
  } catch (err) {
    console.error(err);
